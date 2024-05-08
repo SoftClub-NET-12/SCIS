0af5062094e39115231cbd2e751c74639b626f1c
Barotov Mustafo <barotov2075@gmail.com>
2024-05-08 17:09:14 +0500

    Mustafo Create All File

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -3,27 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class DataContext:DbContext
+public class DataContext : DbContext
 {
 
-    public DataContext(DbContextOptions<DataContext> options) :base(options)
-    {
-        
-    }
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Location> Locations { get; set; }
-    public DbSet<PriceHistory> PriceHistories { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductSupplier> ProductSuppliers { get; set; }
-    public DbSet<Purchase> Purchases { get; set; }
-    public DbSet<Sale> Sales { get; set; }
-    public DbSet<Stock> Stocks { get; set; }
-    public DbSet<StockProduct> StockProducts { get; set; }
-    public DbSet<Supplier> Suppliers { get; set; }
-    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Location> Locations { get; set; }= null!;
+    public DbSet<PriceHistory> PriceHistories { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<ProductSupplier> ProductSuppliers { get; set; } = null!;
+    public DbSet<Purchase> Purchases { get; set; } = null!;
+    public DbSet<Sale> Sales { get; set; } = null!;
+    public DbSet<Stock> Stocks { get; set; } = null!;
+    public DbSet<StockProduct> StockProducts { get; set; } = null!;
+    public DbSet<Supplier> Suppliers { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-    }
+      =>  base.OnModelCreating(modelBuilder);
+    
 }
