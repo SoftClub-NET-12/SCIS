0f7b36dbbd85c236fcc6b55ac494bc878382fe9c
Sabriddin <turaevsabriddin53@gmail.com>
2024-05-09 15:52:51 +0500

    test

--- Infrastructure/Services/UserService/IUserService.cs
+++ Infrastructure/Services/UserService/IUserService.cs
@@ -10,9 +10,6 @@ public interface IUserService
     Task<PagedResponse<List<GetUserDto>>> GetUsersAsync(UserFilter filter);
 
     Task<Response<GetUserDto>> GetUserByIdAsync(int userId);
-
-
-
     Task<Response<string>> CreateUserAsync(CreateUserDto user);
     Task<Response<string>> UpdateUserAsync(UpdateUserDto user);
     Task<Response<bool>> RemoveUserAsync(int UserId);
